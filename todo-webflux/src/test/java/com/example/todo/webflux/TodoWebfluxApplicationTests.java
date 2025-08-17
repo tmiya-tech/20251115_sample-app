eb7bfeb19f01a13e1eb2788d0f7f7d39b98d8257
Takeshi Miyajima <tmiyajima.sunarch@gmail.com>
2025-08-17 16:01:46 +0900

    Initial commit

---
+++ todo-webflux/src/test/java/com/example/todo/webflux/TodoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
