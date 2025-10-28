f3fb452d3b4326c4c4ad57d03a1e57b9848cac2d
Takeshi Miyajima <tmiyajima.sunarch@gmail.com>
2025-10-28 14:46:33 +0900

    EcBackendClientメソッドにバーチャルスレッド対応を追加

--- sample-web/src/main/java/com/example/sample/web/ec/config/EcConfiguration.java
+++ sample-web/src/main/java/com/example/sample/web/ec/config/EcConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.sample.web.ec.config;
 
 import java.net.http.HttpClient;
+import java.util.concurrent.Executors;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
+import org.springframework.boot.autoconfigure.thread.Threading;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +18,26 @@ import io.micrometer.observation.ObservationRegistry;
 public class EcConfiguration {
 
   @Bean
+  @ConditionalOnThreading(Threading.PLATFORM)
   @EcBackendClient
-  RestClient ecBackendClient(EcProperties ecProperties, ObservationRegistry observationRegistry) {
+  RestClient ecBackendClientPT(EcProperties ecProperties, ObservationRegistry observationRegistry) {
     return RestClient.builder()
         .requestFactory(new JdkClientHttpRequestFactory(HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
+            .build()))
+        .baseUrl(ecProperties.getBackendBaseUrl())
+        .observationRegistry(observationRegistry)
+        .build();
+  }
+
+  @Bean
+  @ConditionalOnThreading(Threading.VIRTUAL)
+  @EcBackendClient
+  RestClient ecBackendClientVT(EcProperties ecProperties, ObservationRegistry observationRegistry) {
+    return RestClient.builder()
+        .requestFactory(new JdkClientHttpRequestFactory(HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .executor(Executors.newVirtualThreadPerTaskExecutor()) // バーチャルスレッドを使用
             .build()))
         .baseUrl(ecProperties.getBackendBaseUrl())
         .observationRegistry(observationRegistry)
